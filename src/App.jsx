d0a8b267657488527e0aafb82d28c0b97b10662c
Mayank Verma <124288206+Mayank-5362@users.noreply.github.com>
2025-02-23 10:17:51 +0530

    Initial commit of weather app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Weather from './Weather'
+
+const App = () => {
+  return (
+    <div>
+      <div className='app'>
+       <Weather></Weather>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
